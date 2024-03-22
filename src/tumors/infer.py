b37efd02255d5cd87addbfd8abe7376ca714cd1b
TheauBlanchard <blanchard.theau@live.fr>
2024-03-22 15:39:04 +0100

    Refactor tumor inference script and add data loading functionality

--- src/tumors/infer.py
+++ src/tumors/infer.py
@@ -1,38 +1,65 @@
 from tqdm import tqdm
-import pickle
 import pandas as pd
 import torch
 from deepmil import MLP
 from dataset import SignaturetDataset
+import numpy as np
+from pathlib import Path
 
-MODEL_PATH = ""  # TO FILL
-SAVE_DIR = ""  # TO FILL
-DEVICE = "cuda:0"
+PARAMS = {
+    "batch_size": 4096,
+    "n_ep": 20,
+    "lr": 1.0e-4,
+    # "n_tiles": 8_000,
+    "n_tiles": 20,
+    "n_workers": 0,
+    "wd": 0,
+    "device": "cuda:0",
+    "display": True,
+    "model_path": "path/to/model.pth",
+    "export_path": "path/to/export",
+}
+
+
+PATH_FEATURES_DIR = Path(r"C:\Users\inserm\Documents\histo_sign\dataset\features_panc_224_ctranspath")
+# PATH_FEATURES_DIR = Path(r"C:\Users\inserm\Documents\histo_sign\dataset\features_panc_224_cnn")
+PATH_SUMMARY_DATA = Path(r"C:\Users\inserm\Documents\histo_sign\dataset\panc_summary_vst.csv")
+PATH_TUM_ANNOT = Path(r"C:\Users\inserm\Documents\histo_sign\dataset\coordinates_panc_224")
 
 
 def main():
-    dataset = SignaturetDataset()
-    X, X_coords, X_slidenames, _ = dataset.load_features(n_tiles=100, tumor_filter=False)
-
-    model = MLP(
-        in_features=X[0].shape[-1], out_features=1, hidden=[128], activation=torch.nn.ReLU()
+    dataset = SignaturetDataset(
+        PATH_SUMMARY_DATA=PATH_SUMMARY_DATA,
+        PATH_FEATURES_DIR=PATH_FEATURES_DIR,
+        PATH_TUM_ANNOT=PATH_TUM_ANNOT,
     )
-    model.load_state_dict(torch.load(MODEL_PATH))
-    model.to(DEVICE)
+
+    print("Loading data...")
+    X_train, y_train, ids_train, X_val, y_val, ids_val = dataset.get_data_tumors(n_tiles=PARAMS["n_tiles"])
+
+    model = MLP(in_features=X_train[0].shape[-1], out_features=1, hidden=[128], activation=torch.nn.ReLU())
+    model = model.to(PARAMS["device"])
+    model.load_state_dict(torch.load(PARAMS["model_path"]))
+    model.sigmoid = torch.nn.Sigmoid()
+    model = model.to(PARAMS["device"])
     model.eval()
 
-    preds = {}
-    for x, coord, slidename in tqdm(zip(X, X_coords, X_slidenames)):
-        x = torch.from_numpy(x).to(DEVICE)
-        with torch.inference_mode():
-            preds_ = model.forward(x)
-            preds_ = preds_.cpu().numpy().squeeze()
-            preds_ = pd.DataFrame(
-                {"z": coord[:, 0], "x": coord[:, 1], "y": coord[:, 2], "pred": preds_}
-            )
-            preds[slidename] = preds_
+    unique_samples_train = np.unique(ids_train)
+    unique_samples_val = np.unique(ids_val)
 
-    pickle.dump(preds, open(f"{SAVE_DIR}/tumor_preds.pkl", "wb"))
+    def process_patient(patient, X, y):
+        idx_patient = np.where(ids_train == patient)[0]
+        X = X_train[idx_patient].unsqueeze(0)
+        X = torch.from_numpy(X).to(PARAMS["device"])
+        y = y_train[idx_patient]
+        preds = model.forward(X).cpu().numpy().squeeze()
+        preds = pd.DataFrame({"pred": preds, "y": y})
+        preds.to_csv(f"{PARAMS['export_path']}/{patient}/tum_preds.csv", index=False)
+
+    for patient in tqdm(unique_samples_train, desc="Processing train"):
+        process_patient(patient, X_train, y_train)
+    for patient in tqdm(unique_samples_val, desc="Processing val"):
+        process_patient(patient, X_val, y_val)
 
 
 if __name__ == "__main__":
